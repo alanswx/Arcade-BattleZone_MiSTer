3f80bdda3c7eb837553d9434912e5b90818d7cb4
Alan Steremberg <alan.steremberg@gmail.com>
2020-12-23 10:14:56 -0800

    more cleanup

--- rtl/backup/coordinateIndexer.sv
+++
@@ -1,9 +0,0 @@
-module coordinateIndexer(
-	input 	[9:0] 	x,
-   input 	[8:0]   	y,
-   output  [18:0] 	index
-);
-   
-assign index = ({{9{1'b0}},{x}}) + ({{1'b0},{y},{9{1'b0}}}) + ({{3{1'b0}},{y},{7{1'b0}}});
-   
-endmodule 
